2452a21f7cea4d413ed48ab3ffd23345c7f6129b
MohdShahnawaz7465 <shane74570@gmail.com>
2026-06-22 23:02:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -475,5 +475,7 @@ def logout():
     return redirect(url_for("login"))
 
 
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    with app.app_context():
+        init_db()
+    app.run(host='0.0.0.0', port=5000)
